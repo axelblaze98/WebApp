09f65af250db72173a6d449a54d43aa0658920d9
AxelBlaze98 <paarthbhandari98@gmail.com>
2020-05-17 21:38:45 +0530

    Error Fixed

--- Routes/route.js
+++ Routes/route.js
@@ -1,56 +1,56 @@
 const AWS = require("aws-sdk");
-var express = require("express")
-var route = express.Router()
+var express = require("express");
+var route = express.Router();
 
 AWS.config.loadFromPath("./config.json");
 var documentClient = new AWS.DynamoDB.DocumentClient();
 
 route.get("/Twitter", function (res, req) {
-    var params = {
-        TableName: "twitter-sentiment-data"
-    };
+  var params = {
+    TableName: "twitter-sentiment-data",
+  };
 
-    documentClient.scan(params, onScan);
+  documentClient.scan(params, onScan);
 
-    function onScan(err, data) {
-        if (err) {
-            console.error("Unable to scan", JSON.stringify(err, null, 2));
-        } else {
-            req.send(data.Items);
-        }
+  function onScan(err, data) {
+    if (err) {
+      res.send("Unable to scan");
+    } else {
+      res.send(data.Items);
     }
+  }
 });
 
-route.get("/Currency", function (res, req) {
-    var params = {
-        TableName: "Fixer1"
-    };
+route.get("/Currency", function (req, res) {
+  var params = {
+    TableName: "Fixer1",
+  };
 
-    documentClient.scan(params, onScan);
+  documentClient.scan(params, onScan);
 
-    function onScan(err, data) {
-        if (err) {
-            console.error("Unable to scan", JSON.stringify(err, null, 2));
-        } else {
-            req.send(data.Items);
-        }
+  function onScan(err, data) {
+    if (err) {
+      res.send("Unable to scan");
+    } else {
+      res.send(data.Items);
     }
+  }
 });
 
-route.get("/Synthetic", function (res, req) {
-    var params = {
-        TableName: "synt-data"
-    };
+route.get("/Synthetic", function (req, res) {
+  var params = {
+    TableName: "synt-data",
+  };
 
-    documentClient.scan(params, onScan);
+  documentClient.scan(params, onScan);
 
-    function onScan(err, data) {
-        if (err) {
-            console.error("Unable to scan", JSON.stringify(err, null, 2));
-        } else {
-            req.send(data.Items);
-        }
+  function onScan(err, data) {
+    if (err) {
+      res.send("Unable to scan");
+    } else {
+      res.send(data.Items);
     }
+  }
 });
 
-module.exports = route
+module.exports = route;
